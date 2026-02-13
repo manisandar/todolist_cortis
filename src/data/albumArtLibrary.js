6cf3f757ecebdbc99774113b99acb338236e69b1
Min Khant Tin <minkhanttin@gmail.com>
2026-02-13 19:55:22 +0700

    coverart path fixing

--- src/data/albumArtLibrary.js
+++ src/data/albumArtLibrary.js
@@ -3,13 +3,13 @@ export const albumArtLibrary = [
     albumName: 'Color Outside the Lines',
     releaseDate: '2025-09-8',
     spotifyLink: 'https://open.spotify.com/artist/1ebt9HnXdyYA6KgLXr1n4P',
-    coverArt: '/public/images/albumnArt/coloroutsidethelines.webp'
+    coverArt: '/images/albumnArt/coloroutsidethelines.webp'
   },
   {
     albumName: 'What You Want',
     releaseDate: '2025-08-18',
     spotifyLink: 'https://open.spotify.com/track/1pFQRreMptp3dkh64WiUNx',
-    coverArt: '/public/images/albumnArt/whatyouwant.jpg'
+    coverArt: '/images/albumnArt/whatyouwant.jpg'
   }
   
 ];
